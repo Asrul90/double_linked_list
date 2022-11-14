a7823f829b12a685474240e3a22a5ac7286579a3
M S I <M S I@MSI>
2022-11-14 20:27:40 +0700

    add case

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -15,9 +15,6 @@ namespace double_linked_list
         public Node next;
         //
         public Node prev;
-        static void Main(string[] args)
-        {
-        }
     }
 
     class DoubleLinkedList
@@ -154,6 +151,97 @@ namespace double_linked_list
                 {
                     Console.WriteLine(currentNode.noMhs + "" + currentNode.name + "\n");
                     currentNode = currentNode.prev;
+                }
+            }
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList obj = new DoubleLinkedList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("(nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the ascending order of roll numbers");
+                    Console.WriteLine("4. View all records in the descending order of roll numbers");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.Write("Enter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.WriteLine("\nEnter yhr roll number of the student" +
+                                    "whose recoard is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.dellNode(rollNo) == false)
+                                    Console.WriteLine("Recoard of found");
+                                else
+                                    Console.WriteLine("Recoard with roll number " + rollNo + "deleted \n");
+
+                            }
+                            break ;
+                        case '3':
+                            {
+                                obj.ascending();
+                            }
+                            break;
+                        case '4':
+                            {
+                                obj.descending();
+                            }
+                            break;
+                        case '5':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList empty");
+                                    break ;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.WriteLine("\nEnter the roll number of student whose record ya want to search: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("(Record found");
+                                    Console.WriteLine("\nRoll number: " + curr.noMhs);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
+                        case '6':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\ncheck for the value  etered");
                 }
             }
         }
